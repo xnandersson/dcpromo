8cdce48fa4daf95deabb606a7619b0ad16c6a1bb
Niklas Andersson <niklas.andersson@openforce.se>
2014-05-01 11:15:12 +0200

    * Fixed resolv.conf

--- dcpromo/modules/dc/manifests/resolvconf.pp
+++ dcpromo/modules/dc/manifests/resolvconf.pp
@@ -6,16 +6,20 @@ class dc::resolvconf {
   # set /files/etc/network/interfaces/iface[2]/dns-search = 127.0.0.1
   #
 
-  augeas { 'interfaces':
-    context => '/files/etc/network/interfaces',
-    changes => [
-      "set iface[2]/dns-nameservers 127.0.0.1",
-      "set iface[2]/dns-search $dnsdomainname",
-    ],
-  }
+  #augeas { 'interfaces':
+  #  context => '/files/etc/network/interfaces',
+  #  changes => [
+  #    "set iface[2]/dns-nameservers 127.0.0.1",
+  #    "set iface[2]/dns-search $dnsdomainname",
+  #  ],
+  #}
 
   file { '/etc/resolv.conf':
     content => template('dc/resolv.conf.erb'),
+  }
+
+  file { '/etc/dhcp/dhclient.conf':
+    content => template('dc/dhclient.conf.vagrant.erb'),
   }
 
 }
