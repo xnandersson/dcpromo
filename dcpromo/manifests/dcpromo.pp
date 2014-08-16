62ce5b58dc836d37b52127739ec7ba82477092ee
Niklas Andersson <niklas.andersson@ericsson.com>
2014-08-16 11:25:55 +0200

    Removed hostname,resolv.conf

--- dcpromo/manifests/dcpromo.pp
+++ dcpromo/manifests/dcpromo.pp
@@ -1,7 +1,7 @@
 include dc::kerberos
-include dc::wireshark
+#include dc::wireshark
 include dc::bind9
-include dc::resolvconf
+#include dc::resolvconf
 include dc::samba
 include dc::dcpromo
-include dc::hostname
+#include dc::hostname
--- dcpromo/modules/dc/manifests/init.pp
+++ dcpromo/modules/dc/manifests/init.pp
@@ -1,8 +1,8 @@
 class dc {
   include dc::bind9
-  include dc::hostname
+  #include dc::hostname
   include dc::samba
   include dc::kerberos
   include dc::dcpromo
-  include dc::wireshark
+  #include dc::wireshark
 }
